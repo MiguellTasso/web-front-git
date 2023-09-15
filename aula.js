dfc33e89a45bd46cc8ccb41c4fefa2b7f97fb00f
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 09:36:49 -0300

    aaaaa

---
+++ aula.js
@@ -0,0 +1,9 @@
+alert("Olá mundo");
+
+document.write("Meu nome é miguel");
+
+console.log("Olá tudo bem")
+
+// criando variavel
+let mensagem = "Olá mundo!";
+console.log("Esse console é umavariavel: ",mensagem); 
