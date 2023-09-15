871125d1c723fcd51b3cd7a49dd6214fcdb7be29
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 09:44:02 -0300

    criando variaveis

--- aula.js
+++ aula.js
@@ -7,3 +7,10 @@ console.log("Olá tudo bem")
 // criando variavel
 let mensagem = "Olá mundo!";
 console.log("Esse console é umavariavel: ",mensagem); 
+
+//criando variavel nome
+let nome = "Miguel";
+console.log("console de nome: ", nome)
+
+let aaaa = "Tasso"
+console.log(aaaa)
